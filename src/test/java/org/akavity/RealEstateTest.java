afd7dc083e25fd83ec2dbc9f7b7e3aa8f2a20f83
Basil Akavity <vakavity@gmail.com>
2024-02-10 21:30:39 +0300

    save

--- src/test/java/org/akavity/RealEstateTest.java
+++ src/test/java/org/akavity/RealEstateTest.java
@@ -4,7 +4,7 @@ import org.akavity.annotations.TestData;
 import org.akavity.models.ApartmentData;
 import org.akavity.steps.MainSteps;
 import org.akavity.steps.RealEstateSteps;
-import org.akavity.steps.SortingResultSteps;
+import org.akavity.steps.SortResultSteps;
 import org.akavity.utils.JsonReader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,10 +12,10 @@ import org.testng.annotations.Test;
 public class RealEstateTest extends BaseTest {
     MainSteps mainSteps = new MainSteps();
     RealEstateSteps realEstateSteps = new RealEstateSteps();
-    SortingResultSteps sortingResultSteps = new SortingResultSteps();
+    SortResultSteps sortResultSteps = new SortResultSteps();
 
     @TestData(jsonFile = "apartmentData", model = "ApartmentData")
-    @Test(description = "Check registration form data",
+    @Test(description = "Check the sorting when searching for an apartment",
             dataProviderClass = JsonReader.class, dataProvider = "getData")
     public void selectApartment(ApartmentData apartmentData) {
         mainSteps.moveToSection(apartmentData.getSection());
@@ -24,7 +24,7 @@ public class RealEstateTest extends BaseTest {
         realEstateSteps.selectCheckboxListItem(apartmentData.getDeskTopRooms(), apartmentData.getNumberOfRooms());
         realEstateSteps.selectValuesOfLimit(apartmentData.getLimitOfPrice(), apartmentData.getMinPrice(), apartmentData.getMaxPrice());
         realEstateSteps.clickShowResultButton();
-        int actual = sortingResultSteps.getPriceFirstFoundObject();
+        int actual = sortResultSteps.getPriceFirstFoundObject();
         int minPrice = Integer.parseInt(apartmentData.getMinPrice());
         int maxPrice = Integer.parseInt(apartmentData.getMaxPrice());
 
