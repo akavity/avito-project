11661334e9b7565fc6051f13eb4ab9886dc6872b
Basil Akavity <vakavity@gmail.com>
2024-02-12 10:09:24 +0300

    added new test selectRoom

--- src/test/java/org/akavity/RealEstateTest.java
+++ src/test/java/org/akavity/RealEstateTest.java
@@ -2,6 +2,7 @@ package org.akavity;
 
 import org.akavity.annotations.TestData;
 import org.akavity.models.ApartmentData;
+import org.akavity.models.RoomData;
 import org.akavity.models.SummerHouseData;
 import org.akavity.steps.MainSteps;
 import org.akavity.steps.RealEstateSteps;
@@ -18,16 +19,16 @@ public class RealEstateTest extends BaseTest {
     @TestData(jsonFile = "apartmentData", model = "ApartmentData")
     @Test(description = "Check the sorting when searching for an apartment",
             dataProviderClass = JsonReader.class, dataProvider = "getData")
-    public void selectApartment(ApartmentData apartmentData) {
-        mainSteps.moveToSection(apartmentData.getSection());
-        realEstateSteps.selectTypeRealEstate(apartmentData.getTypeRealEstate());
-        realEstateSteps.selectBuyOrRent(apartmentData.getIntention());
-        realEstateSteps.selectCheckboxListItem(apartmentData.getDeskTopRooms(), apartmentData.getNumberOfRooms());
-        realEstateSteps.selectValuesOfLimit(apartmentData.getLimitOfPrice(), apartmentData.getMinPrice(), apartmentData.getMaxPrice());
+    public void selectApartment(ApartmentData apartment) {
+        mainSteps.moveToSection(apartment.getSection());
+        realEstateSteps.selectTypeRealEstate(apartment.getTypeRealEstate());
+        realEstateSteps.selectBuyOrRent(apartment.getIntention());
+        realEstateSteps.selectCheckboxListItem(apartment.getDeskTopRooms(), apartment.getNumberOfRooms());
+        realEstateSteps.selectValuesOfLimit(apartment.getLimitOfPrice(), apartment.getMinPrice(), apartment.getMaxPrice());
         realEstateSteps.clickShowResultButton();
         int actual = sortResultSteps.getPriceFirstFoundObject();
-        int minPrice = Integer.parseInt(apartmentData.getMinPrice());
-        int maxPrice = Integer.parseInt(apartmentData.getMaxPrice());
+        int minPrice = Integer.parseInt(apartment.getMinPrice());
+        int maxPrice = Integer.parseInt(apartment.getMaxPrice());
 
         Assert.assertTrue(actual <= maxPrice && actual >= minPrice);
     }
@@ -47,5 +48,33 @@ public class RealEstateTest extends BaseTest {
         int maxPrice = Integer.parseInt(dacha.getMaxPrice());
 
         Assert.assertTrue(actual <= maxPrice && actual >= minPrice);
+    }
+
+    @TestData(jsonFile = "roomData", model = "RoomData")
+    @Test(description = "Check the sorting when searching for a room",
+            dataProviderClass = JsonReader.class, dataProvider = "getData")
+    public void selectRoom(RoomData room) {
+        mainSteps.moveToSection(room.getSection());
+        realEstateSteps.selectDropDownItem(room.getDeskTopApartment(), room.getDropDownRoom());
+        realEstateSteps.selectCheckboxListItem(room.getCheckboxRooms(), room.getNumberOfRooms());
+        realEstateSteps.selectValuesOfLimit(room.getLimitOfArea(), room.getMinArea(), room.getMaxArea());
+        realEstateSteps.selectValuesOfLimit(room.getLimitOfPrice(), room.getMinPrice(), room.getMaxPrice());
+        realEstateSteps.clickShowResultButton();
+
+        int actual = sortResultSteps.getPriceFirstFoundObject();
+        int minPrice = Integer.parseInt(room.getMinPrice());
+        int maxPrice = Integer.parseInt(room.getMaxPrice());
+
+        Assert.assertTrue(actual <= maxPrice && actual >= minPrice);
+    }
+
+    @Test(description = "Check the sorting when searching for a summer house")
+    public void selectArea() {
+        mainSteps.moveToSection("Недвиж");
+        realEstateSteps.selectDropDownItem("Квартира", "Земельный");
+        realEstateSteps.selectCheckboxListItem("Категория земель", "Поселений");
+        realEstateSteps.selectValuesOfSlider("Площадь","40","500");
+        realEstateSteps.selectValuesOfLimit("Цена", "400000", "1000000");
+        realEstateSteps.clickShowResultButton();
     }
 }
