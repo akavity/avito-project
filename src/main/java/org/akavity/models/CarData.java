da80c36cd77c25079dc9078eddc59a6ff0402b33
Basil Akavity <vakavity@gmail.com>
2024-02-13 20:55:03 +0300

    added carData

---
+++ src/main/java/org/akavity/models/CarData.java
@@ -0,0 +1,16 @@
+package org.akavity.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarData {
+    String section;
+    String carBrand;
+    String carModel;
+    String fullCarName;
+    int numberOfArrayElements;
+}
