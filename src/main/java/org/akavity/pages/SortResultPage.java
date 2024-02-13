29bd96815bac4431f2548d840a671d108554655b
Basil Akavity <vakavity@gmail.com>
2024-02-13 20:32:02 +0300

    added new page and methods to SortResult page and steps

--- src/main/java/org/akavity/pages/SortResultPage.java
+++ src/main/java/org/akavity/pages/SortResultPage.java
@@ -7,10 +7,15 @@ import static com.codeborne.selenide.Selenide.$$;
 
 public class SortResultPage {
     private final ElementsCollection foundObjects = $$(By.cssSelector("div[itemtype*='http://schema.org/Product']"));
+    private final ElementsCollection foundObjectsNames = $$(By.cssSelector("h3[itemprop='name']"));
     private final ElementsCollection pricesOfFoundObjects = $$(By.cssSelector("strong[class*='styles-module-root'] span"));
 
     public ElementsCollection getFoundObjects() {
         return foundObjects;
+    }
+
+    public ElementsCollection getFoundObjectsNames() {
+        return foundObjectsNames;
     }
 
     public ElementsCollection getPricesOfFoundObjects() {
