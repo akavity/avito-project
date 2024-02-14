a47cad8f6efa52c251b945d4c8cb4398479a24f5
Basil Akavity <vakavity@gmail.com>
2024-02-14 18:25:38 +0300

    created allCarData, popularCarData

--- src/main/java/org/akavity/models/PopularCarData.java
+++ src/main/java/org/akavity/models/PopularCarData.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CarData {
+public class PopularCarData {
     String section;
     String carBrand;
     String carModel;
