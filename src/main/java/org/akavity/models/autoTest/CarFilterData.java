7e0180eb71a9bc7af2953aa1b8f28657291a5b77
Basil Akavity <vakavity@gmail.com>
2024-02-19 19:32:15 +0300

    added new test selectCarUsingFilters and CarFilterData, carFilterData

---
+++ src/main/java/org/akavity/models/autoTest/CarFilterData.java
@@ -0,0 +1,31 @@
+package org.akavity.models.autoTest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarFilterData {
+    String sectionFirst;
+    String sectionSecond;
+    String typeCarTitle;
+    String typeCar;
+    String inStockTitle;
+    String inStock;
+    String carBrandTitle;
+    String carBrand;
+    String carModelTitle;
+    String carModel;
+    String priceTitle;
+    String minPrice;
+    String maxPrice;
+    String carMileageTitle;
+    String minCarMileage;
+    String maxCarMileage;
+    String driveUnitTitle;
+    String driveUnit;
+    String fullCarName;
+    int numberOfArrayElements;
+}
--- src/test/java/org/akavity/AutoTest.java
+++ src/test/java/org/akavity/AutoTest.java
@@ -2,9 +2,11 @@ package org.akavity;
 
 import org.akavity.annotations.TestData;
 import org.akavity.models.autoTest.CarData;
+import org.akavity.models.autoTest.CarFilterData;
 import org.akavity.models.autoTest.SpecialEquipmentData;
 import org.akavity.models.autoTest.TruckData;
 import org.akavity.steps.RubricatorSteps;
+import org.akavity.steps.SearchFilterSteps;
 import org.akavity.steps.SortResultSteps;
 import org.akavity.utils.JsonReader;
 import org.testng.Assert;
@@ -13,26 +15,26 @@ import org.testng.annotations.Test;
 public class AutoTest extends BaseTest {
     RubricatorSteps rubricatorSteps = new RubricatorSteps();
     SortResultSteps sortResultSteps = new SortResultSteps();
+    SearchFilterSteps searchFilterSteps = new SearchFilterSteps();
 
     @TestData(jsonFile = "carData", model = "CarData", folder = "autoTest")
     @Test(description = "Check the sorting when searching for a car",
             dataProviderClass = JsonReader.class, dataProvider = "getData")
-    public void selectCarModel(CarData carData) {
+    public void selectCarModelUsingRubricator(CarData carData) {
         rubricatorSteps.moveToSection(carData.getSection());
         rubricatorSteps.clickAllItemsButton(carData.getDecision());
         rubricatorSteps.clickRubricatorItem(carData.getCarBrand());
         rubricatorSteps.clickRubricatorItem(carData.getCarModel());
 
         String fullCarName = carData.getFullCarName();
-        int numberOfSortResult = carData.getNumberOfArrayElements();
-
-        Assert.assertTrue(sortResultSteps.eachArrayElementContainsText(fullCarName, numberOfSortResult));
+        int numberOfSortResults = carData.getNumberOfArrayElements();
+        Assert.assertTrue(sortResultSteps.doTitlesContainSpecificName(fullCarName, numberOfSortResults));
     }
 
     @TestData(jsonFile = "specialEquipmentData", model = "SpecialEquipmentData", folder = "autoTest")
     @Test(description = "Check the sorting when searching for a special equipment",
             dataProviderClass = JsonReader.class, dataProvider = "getData")
-    public void selectSpecialEquipment(SpecialEquipmentData equipment) {
+    public void selectSpecialEquipmentUsingRubricator(SpecialEquipmentData equipment) {
         rubricatorSteps.moveToSection(equipment.getFirstSection());
         rubricatorSteps.moveToSection(equipment.getSecondSection());
         rubricatorSteps.moveToSection(equipment.getThirdSection());
@@ -42,14 +44,15 @@ public class AutoTest extends BaseTest {
         rubricatorSteps.clickRubricatorItem(equipment.getEquipmentBrand());
         rubricatorSteps.clickRubricatorItem(equipment.getEquipmentModel());
 
-        Assert.assertTrue(sortResultSteps.eachArrayElementContainsText(equipment.getFullEquipmentName(),
-                equipment.getNumberOfArrayElements()));
+        String fullCarName = equipment.getFullEquipmentName();
+        int numberOfSortResults = equipment.getNumberOfArrayElements();
+        Assert.assertTrue(sortResultSteps.doTitlesContainSpecificName(fullCarName, numberOfSortResults));
     }
 
     @TestData(jsonFile = "truckData", model = "TruckData", folder = "autoTest")
     @Test(description = "Check the sorting when searching for a truck",
             dataProviderClass = JsonReader.class, dataProvider = "getData")
-    public void selectTrucks(TruckData truck) {
+    public void selectTrucksUsingRubricator(TruckData truck) {
         rubricatorSteps.moveToSection(truck.getFirstSection());
         rubricatorSteps.moveToSection(truck.getSecondSection());
         rubricatorSteps.moveToSection(truck.getThirdSection());
@@ -58,7 +61,31 @@ public class AutoTest extends BaseTest {
         rubricatorSteps.clickRubricatorItem(truck.getTruckModel());
         rubricatorSteps.clickRubricatorItem(truck.getWheelFormula());
 
-        Assert.assertTrue(sortResultSteps.eachArrayElementContainsText(truck.getFullEquipmentName(),
-                truck.getNumberOfArrayElements()));
+        String fullCarName = truck.getFullEquipmentName();
+        int numberOfSortResults = truck.getNumberOfArrayElements();
+        Assert.assertTrue(sortResultSteps.doTitlesContainSpecificName(fullCarName, numberOfSortResults));
+    }
+
+    @TestData(jsonFile = "carFilterData", model = "CarFilterData", folder = "autoTest")
+    @Test(description = "Check the sorting when searching for a car",
+            dataProviderClass = JsonReader.class, dataProvider = "getData")
+    public void selectCarUsingFilters(CarFilterData car) {
+        rubricatorSteps.moveToSection(car.getSectionFirst());
+        rubricatorSteps.moveToSection(car.getSectionSecond());
+        searchFilterSteps.clickButtonOrCheckbox(car.getTypeCarTitle(), car.getTypeCar());
+        searchFilterSteps.clickButtonOrCheckbox(car.getInStockTitle(), car.getInStock());
+        searchFilterSteps.clickDropdownCheckbox(car.getCarBrandTitle(), car.getCarBrand());
+        searchFilterSteps.clickDropdownCheckbox(car.getCarModelTitle(), car.getCarModel());
+        searchFilterSteps.setValuesOfLimit(car.getPriceTitle(), car.getMinPrice(), car.getMaxPrice());
+        searchFilterSteps.setValuesOfDropDownLimit(car.getCarMileageTitle(), car.getMinCarMileage(), car.getMaxCarMileage());
+        searchFilterSteps.clickButtonOrCheckbox(car.getDriveUnitTitle(), car.getDriveUnit());
+        searchFilterSteps.clickResultButton();
+
+        int actual = sortResultSteps.getPriceFirstFoundObject();
+        int minPrice = Integer.parseInt(car.getMinPrice());
+        int maxPrice = Integer.parseInt(car.getMaxPrice());
+
+        Assert.assertTrue(actual <= maxPrice && actual >= minPrice);
+        Assert.assertTrue(sortResultSteps.doTitlesContainSpecificName(car.getFullCarName(), car.getNumberOfArrayElements()));
     }
 }
