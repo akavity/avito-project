22605b296996a6fe721972ae818cfccb8d728a0b
Basil Akavity <vakavity@gmail.com>
2024-02-19 19:33:58 +0300

    added new util

--- src/main/java/org/akavity/utils/Utils.java
+++ src/main/java/org/akavity/utils/Utils.java
@@ -1,5 +1,11 @@
 package org.akavity.utils;
 
+import com.codeborne.selenide.ElementsCollection;
+import lombok.extern.log4j.Log4j2;
+
+import java.util.ArrayList;
+import java.util.List;
+@Log4j2
 public class Utils {
     public void sleep() {
         try {
@@ -15,5 +21,24 @@ public class Utils {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public boolean doElementsContainText(ElementsCollection collection, String text, int elements) {
+        boolean result = true;
+        if (collection.isEmpty()) {
+            log.info("Collection is empty");
+            result = false;
+        } else {
+            List<String> names = new ArrayList<>(collection.first(elements).texts());
+            for (String name : names) {
+                log.info("Array names contains name: " + name);
+                if (!name.contains(text)) {
+                    log.info("/// Object name dose not contain text: " + text);
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
