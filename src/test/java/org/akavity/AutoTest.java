f9e341c32a8cc983db599fd792432348660517a1
Basil Akavity <vakavity@gmail.com>
2024-02-20 22:16:26 +0300

    added selectCarUsingFiltersTest2

--- src/test/java/org/akavity/AutoTest.java
+++ src/test/java/org/akavity/AutoTest.java
@@ -1,16 +1,16 @@
 package org.akavity;
 
 import org.akavity.annotations.TestData;
-import org.akavity.models.autoTest.CarData;
-import org.akavity.models.autoTest.CarFilterData;
-import org.akavity.models.autoTest.SpecialEquipmentData;
-import org.akavity.models.autoTest.TruckData;
+import org.akavity.models.autoTest.*;
 import org.akavity.steps.RubricatorSteps;
 import org.akavity.steps.SearchFilterSteps;
 import org.akavity.steps.SortResultSteps;
 import org.akavity.utils.JsonReader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AutoTest extends BaseTest {
     RubricatorSteps rubricatorSteps = new RubricatorSteps();
@@ -67,9 +67,10 @@ public class AutoTest extends BaseTest {
     }
 
     @TestData(jsonFile = "carFilterData", model = "CarFilterData", folder = "autoTest")
-    @Test(description = "Check the sorting when searching for a car",
+    @Test(description = "Select car using filters: type car, in stock, car brand, car model, min and max price, " +
+            "min and max car mileage, drive unit",
             dataProviderClass = JsonReader.class, dataProvider = "getData")
-    public void selectCarUsingFilters(CarFilterData car) {
+    public void selectCarUsingFiltersTest1(CarFilterData car) {
         rubricatorSteps.moveToSection(car.getSectionFirst());
         rubricatorSteps.moveToSection(car.getSectionSecond());
         searchFilterSteps.clickButtonOrCheckbox(car.getTypeCarTitle(), car.getTypeCar());
@@ -87,5 +88,29 @@ public class AutoTest extends BaseTest {
 
         Assert.assertTrue(actual <= maxPrice && actual >= minPrice);
         Assert.assertTrue(sortResultSteps.doTitlesContainSpecificName(car.getFullCarName(), car.getNumberOfArrayElements()));
+    }
+
+    @TestData(jsonFile = "carFilterTwoData", model = "CarFilterTwoData", folder = "autoTest")
+    @Test(description = "Select car using filters: price, year of issue, car mileage, drive unit, car condition, body style",
+            dataProviderClass = JsonReader.class, dataProvider = "getData")
+    public void selectCarUsingFiltersTest2(CarFilterTwoData car) {
+        rubricatorSteps.moveToSection(car.getSectionFirst());
+        rubricatorSteps.moveToSection(car.getSectionSecond());
+        searchFilterSteps.setValuesOfLimit(car.getPriceTitle(), car.getMinPrice(), car.getMaxPrice());
+        searchFilterSteps.setValuesOfLimit(car.getYearOfIssueTitle(), car.getMinYearOfIssue(), car.getMaxYearOfIssue());
+        searchFilterSteps.setValuesOfDropDownLimit(car.getCarMileageTitle(), car.getMaxCarMileage());
+        searchFilterSteps.clickButtonOrCheckbox(car.getDriveUnitTitle(), car.getDriveUnit());
+        searchFilterSteps.clickButtonOrCheckbox(car.getEngineTypeTitle(), car.getEngineType());
+        searchFilterSteps.clickButtonOrCheckbox(car.getCarConditionTitle(), car.getCarCondition());
+        searchFilterSteps.clickButtonOrCheckbox(car.getBodyStyleTitle(), car.getBodyStyle());
+        searchFilterSteps.clickResultButton();
+
+        int actual = sortResultSteps.getPriceFirstFoundObject();
+        int minPrice = Integer.parseInt(car.getMinPrice());
+        int maxPrice = Integer.parseInt(car.getMaxPrice());
+
+        Assert.assertTrue(actual <= maxPrice && actual >= minPrice);
+        List<String> parameters = new ArrayList<>(car.getParameters());
+        Assert.assertTrue(sortResultSteps.doDescriptionsContainText(parameters, car.getNumberOfArrayElements()));
     }
 }
